24ef35cb0c4eec3714864d57c0c2197ae0f5ceae
Nikhilesh Mishra <mishrajinikhil8@gmail.com>
2025-03-24 10:58:58 +0530

    some array problems solved

---
+++ 4_Arrays/1. Easy/02_Shift_Negatives.cpp
@@ -0,0 +1,87 @@
+/*
+Problem Description: Move all negative elements to the left and positives to the right irrespective of there order
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Approach 01 --> Partition algo --> O(n) */
+
+void shiftNegatives_A(vector<int> &arr){
+    int n = arr.size();
+
+    int j = -1, pivot = 0;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] < pivot){
+            j++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+}
+
+/* Approach 02 --> Two pointer technique --> O(n) */
+
+void shiftNegatives_B(vector<int> &arr){
+
+    int left = 0, right = arr.size() - 1;
+
+    while(left < right){
+        if(arr[left] < 0 && arr[right] < 0){
+            left++;
+        }
+        else if(arr[left] > 0 && arr[right] > 0){
+            right--;
+        }
+        else if(arr[left] > 0 && arr[right] < 0){
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+        else{
+            left++;
+            right--;
+        }
+    }
+}
+
+void printArray(vector<int> &arr){
+    for(const int &x: arr) cout << x << " ";
+    cout << endl;
+}
+
+int main(){
+
+    int size;
+
+    cout << "Enter size of the array: " << endl;
+
+    cin >> size;
+    
+    vector<int> arr;
+
+    int item;
+
+    while(size--){
+
+        cin >> item;
+        arr.push_back(item);
+
+    }
+
+    cout << "Original array: ";
+
+    printArray(arr);
+
+    // shiftNegatives_A(arr);
+
+    shiftNegatives_B(arr);
+
+    cout << "After shifting negatives to the left: ";
+
+    printArray(arr);
+
+    return 0;
+}
---
+++ 4_Arrays/1. Easy/03_Move_All_Zeroes_To_The_End.cpp
@@ -0,0 +1,126 @@
+/*
+    Problem Description: The task is to move all the zeros in the array to the right end 
+    while maintaining the relative order of the non-zero elements.
+
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushZeroesToEnd(vector<int> &arr){
+
+    int n = arr.size();
+    int i = 0, j = 0;
+
+    while(i < n){
+        if(arr[j] != 0){
+            i++;
+            j++;
+        }
+
+        else if(arr[i] > 0 && arr[j] == 0){
+            swap(arr[i], arr[j]);
+            i++;
+            j++;
+        }
+
+        else{
+            i++;
+        }
+    }
+
+}
+
+void printArray(vector<int> &arr){
+    for(const int &x: arr) cout << x << " ";
+    cout << endl;
+}
+
+int main(){
+
+    int size;
+
+    cout << "Enter size of the array: ";
+
+    cin >> size;
+
+    vector<int> arr;
+
+    int item;
+
+    while(size--){
+        cin >> item;
+        arr.push_back(item);
+    }
+
+    cout << "Original array: ";
+
+    printArray(arr);
+
+    pushZeroesToEnd(arr);
+
+    cout << "After moving zeroes to the end: ";
+
+    printArray(arr);
+
+    return 0;
+}
+
+/*
+    Logic Building:
+    1 2 0 4 3 0 5 0
+          j i
+    swap j and i
+    j++;
+    i++;
+
+    1 2 4 0 3 0 5 0
+            j i  
+    swap j and i
+    j++;
+    i++;
+
+    1 2 4 3 0 0 5 0
+            j i
+    i++;
+
+    1 2 4 3 0 0 5 0
+            j   i
+    swap j and i
+    j++;
+    i++;
+        
+    1 2 4 3 5 0 0 0
+              j   i
+    since i reached at the end loop stops
+
+
+    rough code:
+    initially i = 0; j = 0;
+
+    if arr[i] > 0 && arr[j] > 0
+    i++;
+    j++;
+
+    if arr[i] == 0 && arr[j] > 0
+    i++;
+    j++;
+
+    if arr[i] > 0 && arr[j] == 0
+    swap(arr[i], arr[j]);
+    i++;
+    j++;
+
+    if arr[i] == 0 && arr[j] == 0
+    i++;
+
+
+
+    In above we can make an improvement:
+    Instead of first and second if conditions we can write only if condition like this:
+    if(arr[j] != 0)
+    i++;
+    j++;
+*/
